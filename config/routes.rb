e45333d26e47092f7a1003a7bba7d523017cb28f
gina chen <gina@mrswordsmith.com>
2019-05-31 09:25:44 +0100

    products

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  resources :products
    root to: "products#index"
-  resources :products, only: [:index, :show, :new, :create, :destroy, :edit]
     get 'profile', to: 'users#show'
      get 'profile/destroy', to: 'users#destroy'
+     delete "products/:id",  to: "products#destroy
 
  end
 
