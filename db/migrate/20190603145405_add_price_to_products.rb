bf523465d030ffa3f555d7e350bbc020b2af0fa0
gina chen <gina@mrswordsmith.com>
2019-06-03 17:46:47 +0100

    migrate

--- db/migrate/20190603145405_add_price_to_products.rb
+++
@@ -1,5 +0,0 @@
-class AddPriceToProducts < ActiveRecord::Migration[5.2]
-  def change
-    add_column :products, :price, :decimal, precision: 12, scale: 3
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,8 @@
 
 
 
-# Product.delete_all
-# User.delete_all
+Product.delete_all
+User.delete_all
 
 # puts "---------------------------------------------------------"
 # puts "creating users"
@@ -90,9 +90,9 @@
 #   )
 
 
-# OrderStatus.delete_all
-OrderStatus.create! id: 1, name: "In Progress"
-OrderStatus.create! id: 2, name: "Placed"
-OrderStatus.create! id: 3, name: "Shipped"
-OrderStatus.create! id: 4, name: "Cancelled"
+OrderStatus.delete_all
+# OrderStatus.create! id: 1, name: "In Progress"
+# OrderStatus.create! id: 2, name: "Placed"
+# OrderStatus.create! id: 3, name: "Shipped"
+# OrderStatus.create! id: 4, name: "Cancelled"
 
